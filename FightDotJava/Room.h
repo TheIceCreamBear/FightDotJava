cf1d57d3bdea3e42ade2b0cb463fe9c4523d8151
Kartingman25 <kartingman25@gmail.com>
2018-02-15 13:42:12 -0500

    default room constructor

--- FightDotJava/Room.h
+++ FightDotJava/Room.h
@@ -6,6 +6,7 @@
 
 class Room {
 public:
+	Room() = default;
 	Room(Location loc, std::string description, bool doors[4]);
 	bool canLeaveFrom(Direction d) const;
 	std::string getDescription() const;
