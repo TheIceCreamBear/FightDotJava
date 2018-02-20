9ddbc996cd40c533e3fd4e5e107007145d7ef7f8
Kartingman25 <kartingman25@gmail.com>
2018-02-19 23:46:53 -0500

    Comments about temp stuffs

--- FightDotJava/InitRefrence.h
+++ FightDotJava/InitRefrence.h
@@ -8,15 +8,14 @@ namespace Refrence {
 		"Bed room. This must be where the ruler sleeps.", "Throne Room. Who ever rules this place is large and wealthy.", "Chest storage Room. There might be valuables in these chests.",
 		"Unknow purpose room. There is a shrine to an unknow diety in here as well as several mysitcal devices...", "Centeral Hall. Quiet, but bright and livid.", "Dining room. There is a long table, with a full meal on the table, but no one to eat it.",
 		"Common room. There is a warm fire in here, but there are also some very intimidating paintings.", "Entrance Hallway. Large banners of some unknown faction hang from the ceiling.", "Kitchen. There are by products and utensils still out from the making of the last meal but still no people..."
-
-	};
+	}; // this kind of declaroation is temporary
 
 	// UP, LEFT, RIGHT, DOWN
 	bool roomDoors[Constants::GAME_HEIGHT * Constants::GAME_WITDH][4] = {
-		{ false, false, false, true },{ false, false, false, true },{ false, false, false, true },
-		{ true, false, true, true },{ true, true, true, true },{ true, true, false, true },
-		{ true, false, true, false },{ true, true, false, true },{ true, false, false, false }
-	};
+		{false, false, false, true}, {false, false, false, true}, {false, false, false, true},
+		{true, false, true, true}, {true, true, true, true}, {true, true, false, true},
+		{true, false, true, false}, {true, true, false, true}, {true, false, false, false}
+	}; // this kind of declaroation is temporary
 
 	std::string getDescriptionForRoom(int id) {
 		return descriptions[id];
