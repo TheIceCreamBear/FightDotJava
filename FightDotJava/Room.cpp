95a37effb4a7d2c16969795e4dafca95e900f644
Kartingman25 <kartingman25@gmail.com>
2018-02-20 14:21:03 -0500

    Update Room interface

--- FightDotJava/Room.cpp
+++ FightDotJava/Room.cpp
@@ -1,27 +1,51 @@
 #include "Room.h"
 
-Room::Room(Location loc, std::string description, bool doors[4]) {
+Room::Door::Door(bool exists, bool locked) {
+	this->exists = exists;
+	this->locked = locked;
+}
+
+Room::Room(RoomType type, Location loc) {
 	this->loc = loc;
-	this->description = description;
-	up = doors[0];
-	left = doors[1];
-	right = doors[2];
-	down = doors[3];
+	this->type = type;
+	switch (type) {
+		case Room::RoomType::ENTRANCE:
+
+			break;
+		case Room::RoomType::CENTRAL:
+
+			break;
+		case Room::RoomType::HALLWAY:
+
+			break;
+		case Room::RoomType::CHEST:
+
+			break;
+		case Room::RoomType::MYSTIC:
+
+			break;
+		default:
+			this->dUp = Door(false, false);
+			this->dLeft = Door(false, false);
+			this->dRight = Door(false, false);
+			this->dDown = Door(false, false);
+			break;
+	}
 }
 
 bool Room::canLeaveFrom(Direction d) const {
 	switch (d) {
 		case UP:
-			return up;
+			return dUp.doesExist() && !dUp.isLocked();
 			break;
 		case LEFT:
-			return left;
+			return dLeft.doesExist() && !dLeft.isLocked();
 			break;
 		case RIGHT:
-			return right;
+			return dRight.doesExist() && !dRight.isLocked();
 			break;
 		case DOWN:
-			return down;
+			return dDown.doesExist() && !dDown.isLocked();
 			break;
 		default:
 			break;
@@ -37,16 +61,18 @@ Location Room::getLocation() const {
 	return loc;
 }
 
-void Room::init(Location loc, std::string description, bool doors[4]) {
+Room::RoomType Room::getRoomType() const {
+	return type;
+}
+
+void Room::initDoors(Door u, Door l, Door r, Door d) {
 	if (initialized) {
 		return;
 	}
 	initialized = true;
 
-	this->loc = loc;
-	this->description = description;
-	up = doors[0];
-	left = doors[1];
-	right = doors[2];
-	down = doors[3];
+	this->dUp = u;
+	this->dLeft = l;
+	this->dRight = r;
+	this->dDown = d;
 }
--- FightDotJava/Room.h
+++ FightDotJava/Room.h
@@ -5,20 +5,37 @@
 #include <string>
 
 class Room {
-public:
+public: // prototype innerclasses
+	enum class RoomType {
+		ENTRANCE, CENTRAL, HALLWAY, CHEST, MYSTIC
+	};
+private: // prototype innerclasses
+	class Door {
+	public:
+		Door() : Door(false, false) {} // Default constructor set to false
+		Door(bool exists, bool locked);
+		bool doesExist() const { return exists; }
+		bool isLocked() const { return locked; }
+	private:
+		bool exists;
+		bool locked;
+	};
+public: // all public members
 	Room() = default;
-	Room(Location loc, std::string description, bool doors[4]);
+	Room(RoomType type, Location loc);
 	bool canLeaveFrom(Direction d) const;
 	std::string getDescription() const;
 	Location getLocation() const;
-	void init(Location loc, std::string description, bool doors[4]);
+	RoomType getRoomType() const;
+	void initDoors(Door u, Door l, Door r, Door d);
 
-private:
+private: // all private member variables
 	Location loc;
 	std::string description;
-	bool up;
-	bool left;
-	bool right;
-	bool down;
+	RoomType type;
 	bool initialized = false;
+	Door dUp;
+	Door dLeft;
+	Door dRight;
+	Door dDown;
 };
