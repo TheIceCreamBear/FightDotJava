fcff7e464b0687612c1140fb743d04d12dc347a7
Kartingman25 <kartingman25@gmail.com>
2018-03-07 22:05:25 -0500

    Change rn Gen to be in constants

--- FightDotJava/Constants.h
+++ FightDotJava/Constants.h
@@ -1,10 +1,17 @@
 #pragma once
+#include <random>
 
 namespace Constants {
 	static constexpr int GAME_WITDH = 5;
 	static constexpr int GAME_HEIGHT = 5;
 	static const int playerStartX = 0;
 	static const int playerStartY = 0;
+
+	// If zero, use time, if not zero, use this
+	static const long long reccomendSeed = 15198726674730761; // decent seed for 5x5
+	
+	static std::mt19937 rng = std::mt19937(0);
+	static long long seed = 0; // dynamic
 
 	static int recommendedStartX = 0;
 	static int recommendedStartY = 0;
